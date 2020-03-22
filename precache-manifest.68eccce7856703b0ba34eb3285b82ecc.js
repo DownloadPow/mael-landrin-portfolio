770a65326430a32f4ebe87dfc9b7256f3c60219e
Mael Landrin <mael.landrin@gmail.com>
2020-03-22 17:46:47 +0100

    Updates

--- precache-manifest.68eccce7856703b0ba34eb3285b82ecc.js
+++ precache-manifest.68eccce7856703b0ba34eb3285b82ecc.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "974a0ef3fd410763d79382facae521c1",
+    "revision": "efdb1f8834530ab650f6fdab93857906",
     "url": "/mael-landrin/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/mael-landrin/static/css/2.f26a4486.chunk.css"
   },
   {
-    "revision": "690ed530b128facf7b92",
-    "url": "/mael-landrin/static/css/main.9206e8e7.chunk.css"
+    "revision": "91ef603b22e9fbbac197",
+    "url": "/mael-landrin/static/css/main.1ced5db7.chunk.css"
   },
   {
     "revision": "d97c9e68b3942a838b37",
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/mael-landrin/static/js/2.3c81da88.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "690ed530b128facf7b92",
+    "revision": "91ef603b22e9fbbac197",
     "url": "/mael-landrin/static/js/main.e9ab2e1b.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mael-landrin/precache-manifest.1b053250550d582114a2768696d3119e.js"
+  "/mael-landrin/precache-manifest.68eccce7856703b0ba34eb3285b82ecc.js"
 );
 
 self.addEventListener('message', (event) => {
