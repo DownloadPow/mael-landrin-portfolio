56cde286b66f55cf6a5808dbb70d61cf9bfee41c
Mael Landrin <mael.landrin@gmail.com>
2020-03-22 17:27:14 +0100

    Updates

---
+++ precache-manifest.8b9169d3b8ff5237c58c28a108fdcc58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be67158676e09f751cf1989a33efecd1",
+    "url": "/mael-landrin/index.html"
+  },
+  {
+    "revision": "d97c9e68b3942a838b37",
+    "url": "/mael-landrin/static/css/2.f26a4486.chunk.css"
+  },
+  {
+    "revision": "8ce8c753cd292691dd54",
+    "url": "/mael-landrin/static/css/main.76a868ee.chunk.css"
+  },
+  {
+    "revision": "d97c9e68b3942a838b37",
+    "url": "/mael-landrin/static/js/2.3c81da88.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/mael-landrin/static/js/2.3c81da88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ce8c753cd292691dd54",
+    "url": "/mael-landrin/static/js/main.b61eccc5.chunk.js"
+  },
+  {
+    "revision": "3d11dd024481e26e909d",
+    "url": "/mael-landrin/static/js/runtime-main.7a1db760.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/mael-landrin/precache-manifest.8b9169d3b8ff5237c58c28a108fdcc58.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mael-landrin/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
